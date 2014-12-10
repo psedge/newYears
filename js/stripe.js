f13dac3d4b0ad3772e2aa36be4670010a6b5bb29
chrisvasey <chrismvasey@gmail.com>
2014-12-10 21:48:00 +0000

    Added all stripe payment stuff

--- js/stripe.js
+++ js/stripe.js
@@ -7,18 +7,99 @@
     }
   });
 
+
+//reward1
 $(document).ready(function(){
-  $('#thing').on('click', function(e) {
+  $('#tier1').on('click', function(e) {
     // Open Checkout with further options
     handler.open({
-      name: 'Give uz a quid m8',
-      description: 'Thank yo!',
-      amount: 10
+      name: 'You are donating 1 whole pound',
+      description: 'Thank you!',
+      amount: 100
     });
     e.preventDefault();
   });
 });
-  // Close Checkout on page navigation
-  $(window).on('popstate', function() {
-    handler.close();
+
+//reward2
+$(document).ready(function(){
+  $('#tier2').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: '2 Down it Tokens',
+      description: 'You are donating 5 whole pounds.',
+      amount: 500
+    });
+    e.preventDefault();
   });
+});
+
+//reward3
+$(document).ready(function(){
+  $('#tier3').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Get out of jail free card',
+      description: 'You are donating 10 whole pounds.',
+      amount: 1000
+    });
+    e.preventDefault();
+  });
+});
+
+//reward4
+$(document).ready(function(){
+  $('#tier4').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Dominion over Worms',
+      description: 'You are donating 15 whole pounds',
+      amount: 1500
+    });
+    e.preventDefault();
+  });
+});
+
+//reward5
+$(document).ready(function(){
+  $('#tier5').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'The Vibes Captain',
+      description: 'You are donating 15 whole pounds',
+      amount: 1500
+    });
+    e.preventDefault();
+  });
+});
+
+//reward6
+$(document).ready(function(){
+  $('#tier6').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Be Mike Jones',
+      description: 'You are donating 15 whole pounds',
+      amount: 1500
+    });
+    e.preventDefault();
+  });
+});
+
+//reward7
+$(document).ready(function(){
+  $('#tier7').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Tweak, the Performing Monkeys Token',
+      description: 'You are donating 15 whole pounds',
+      amount: 1500
+    });
+    e.preventDefault();
+  });
+});
+
+// Close Checkout on page navigation
+$(window).on('popstate', function() {
+  handler.close();
+});
